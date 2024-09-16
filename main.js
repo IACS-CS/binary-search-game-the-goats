87a764a7b2a36745434b3e5e67a54fb9c001ca14
Mason2435 <cayermason8@gmail.com>
2024-09-16 15:40:47 +0000

    mason aka goat

--- main.js
+++ main.js
@@ -1,34 +1,48 @@
 import "./style.css";
 import { TextInterface } from "text-interface";
 
-
 let binary = 0;
-
 
 // Get the element with id="app"
 let app = document.querySelector("#app");
 
 // Create a new "Text Interface"
 
-
 let ti = new TextInterface(app, "MC, AM, AP's, Guess The Sport");
 
-
-ti.output("What is your name?")
+ti.output("What is your name?");
 let userName = await ti.readText();
-ti.output("Nice to meet you, " + userName)
+ti.output("Nice to meet you, " + userName);
 ti.output(userName + ", can you think of a sport to guess?");
-ti.output("The options are: soccer, basketball, football, rugby, baseball, track and field, tennis, volleyball, cricket, hockey, F1, NASCAR, and esports")
+ti.output(
+  "The options are: soccer, basketball, football, rugby, baseball, track and field, tennis, volleyball, cricket, hockey, F1, NASCAR, and esports"
+);
 await ti.readText();
-let CanYouThinkOfASportToGuess = await ti.readYesorNo;       
+let CanYouThinkOfASportToGuess = await ti.readYesorNo;
 
 //first question: Allan, Aditya, Mason
-ti.output(userName + ". Does the sport include balls?")
+ti.output(userName + ". Does the sport include balls?");
 let includesBalls = await ti.readYesOrNo();
 if (includesBalls) {
-  ti.output ("Does the sport use a small or big ball?");
-    
-}else{
-  ti.output("Does the sport include sticks?")
+  ti.output(
+    "Does the sport use a small or big ball? (yes for small ball, no for big ball"
+  );
+  await ti.readYesOrNo();
+  if (includesBalls) {
+    // includes balls
+    ti.output("Does the sport use bats?");
+    await ti.readYesOrNo();
+    if (ti.readYesOrNo()) {
+      ti.output("Is the sport baseball?");
+      await ti.readYesOrNo();
+    } else {
+      // not baseball...
+      ti.output("Is the sport cricket?");
+    }
+  } else {
+   
+  }
+} else {
+  // does not include balls
+  ti.output("Does the sport include sticks?");
 }
-
